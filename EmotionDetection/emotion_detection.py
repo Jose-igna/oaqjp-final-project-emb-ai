204c26574abd538625763ac8d1ea789e3563856a
Jose-igna <jichaj@hotmail.com>
2024-09-16 16:14:05 -0400

    tas4 completed

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -5,4 +5,11 @@ def emotion_detector(text_to_analyse):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     data = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, headers=headers, json=data)
-    return(response.json())
+    answer_dict = response.json().get('emotionPredictions')[0].get('emotion')
+    max_value = 0
+    for key in answer_dict.keys():
+        if answer_dict[key] > max_value:
+            dominant_emotion = key
+            max_value = answer_dict[key]
+    answer_dict['dominant_emotion'] = dominant_emotion
+    return(answer_dict)
